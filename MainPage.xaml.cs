b5087f8387d2f9479ebeb08d58bb7e88b7ff00d3
AVINASH MADDIKONDA <svasssakavi@gmail.com>
2020-07-14 23:39:21 +0530

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,94 @@
+﻿using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Ruqqus
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            setBackgroundAcrylicBrush();
+        }
+
+        // Initialize a NavigationViewItem as "Home" for future uses as the last invoked navigation view item
+        public NavigationViewItem _lastInvokedNavigationViewItem;
+
+        private void setBackgroundAcrylicBrush()
+        {
+            mainPageBackgroundAcrylicBrush.TintColor = Application.Current.RequestedTheme == ApplicationTheme.Light ? Color.FromArgb(255, 255, 255, 255) : Color.FromArgb(255, 0, 0, 0);
+        }
+
+        private void MainPage_ActualThemeChanged(FrameworkElement sender, object args)
+        {
+            setBackgroundAcrylicBrush();
+        }
+
+        private void MainPageNavigationView_Loaded(object sender, RoutedEventArgs e)
+        {
+            mainPageContentFrame.Navigate(typeof(Home));
+        }
+
+        private void MainPageNavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            NavigationViewItem invokedNavigationViewItem = args.InvokedItem as NavigationViewItem;
+
+            if ((invokedNavigationViewItem == null) || (invokedNavigationViewItem == _lastInvokedNavigationViewItem))
+            {
+                return;
+            }
+            
+            if (args.IsSettingsInvoked)
+            {
+                mainPageContentFrame.Navigate(typeof(Settings));
+            }
+            else
+            {
+                switch (invokedNavigationViewItem.Tag.ToString())
+                {
+                    case "Home":
+                        mainPageContentFrame.Navigate(typeof(Home));
+                        break;
+                    
+                    default:
+                        mainPageContentFrame.Navigate(typeof(Home));
+                        break;
+                }
+            }
+            
+            _lastInvokedNavigationViewItem = invokedNavigationViewItem;
+        }
+
+        private void MainPageNavigationView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            if (mainPageContentFrame.CanGoBack)
+            {
+                mainPageContentFrame.GoBack();
+            }
+        }
+
+        private void mainPageAppSearchAutoSuggestBox_TextChanged(AutoSuggestBox sender, AutoSuggestBoxTextChangedEventArgs args)
+        {
+
+        }
+
+        private void mainPageAppSearchAutoSuggestBox_QuerySubmitted(AutoSuggestBox sender, AutoSuggestBoxQuerySubmittedEventArgs args)
+        {
+
+        }
+
+        private void mainPageAppSearchAutoSuggestBox_SuggestionChosen(AutoSuggestBox sender, AutoSuggestBoxSuggestionChosenEventArgs args)
+        {
+
+        }
+
+        private void mainPageAccountButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Settings.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Ruqqus
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Settings : Page
+    {
+        public Settings()
+        {
+            this.InitializeComponent();
+        }
+
+        private void darkModeToggleSwitch_Toggled(object sender, RoutedEventArgs e)
+        {
+            App.Current.RequestedTheme = App.Current.RequestedTheme == ApplicationTheme.Light ? ApplicationTheme.Dark : ApplicationTheme.Light;
+        }
+    }
+}
